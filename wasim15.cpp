b0d2894db4ac6cd73c91c86439381e654d84e771
wasim-1432 <wasim.ahmad0661@gmail.com>
2025-05-11 11:52:03 +0530

    Define a function template to find the greatest element among the values sorted in an array of any type.

---
+++ wasim15.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+template<typename X>
+void Sort_An_Array(X arr[],int size)
+{
+    int round,i;
+    for(round=0;round<size-1;round++)
+    {
+        for(i=0;i<size-round-1;i++)
+        {
+            if(arr[i]>arr[i+1])
+            {
+                X temp=arr[i];
+                arr[i]=arr[i+1];
+                arr[i+1]=temp;
+            }
+        }
+    }
+    cout<<"Greatest element is="<<arr[size-1];
+}
+template<typename X>
+void Input_Elements_In_Array()
+{
+    X arr[10];
+    int i;
+    cout<<"Enter elements"<<endl;
+    for(i=0;i<10;i++)
+    {
+        cin>>arr[i];
+    }
+    Sort_An_Array<X>(arr,10);
+}
+int main()
+{
+    Input_Elements_In_Array<float>();
+    cout<<endl;
+    return 0;
+}
